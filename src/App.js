38ddfdfee12bc3c4b7b2e239cbd487752e79e2e1
Derrick <derrickryan10@gmail.com>
2022-07-06 08:51:51 -0400

    Add color hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
 
   const incrementCount = () => {
     setCount(count + 1);
   };
 
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if(color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
+
   return (
     <div>
       <div>{count}</div>
       <button onClick={incrementCount}>Increment</button>
+
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+
+          backgroundColor: color,
+        }}
+      >Change Color</div>
     </div>
   );
 }
