490a6ce47e7ee15c8bb93a391e2b0d97610cd554
Derrick <derrickryan10@gmail.com>
2022-07-06 09:36:21 -0400

    Add tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Hooks from './Hooks';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [heading, setHeading] = useState("Default Heading");
+
+  const clickHandler = () => {
+    setHeading("New Heading");
+  }
+
   return (
     <div>
-      <h1>Hello from App</h1>
+      <h1>{heading}</h1>
+      <button type="button" onClick={clickHandler}>Click</button>
       <Hooks />
+      {/* <h1>Our First Test</h1> */}
     </div>
   );
 }
